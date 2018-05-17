9fac6885f1dbe19022486f1f935baa8a077e3c90
Ivan Kravets <me@ikravets.com>
2018-05-17 16:26:30 -0700

    Release 10

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ void setup()
   }
 
   server.on ( "/", []() {
-    server.send ( 200, "text/plain", "Welcome to JFrog swampUP! :)" );
+    server.send ( 200, "text/plain", "Welcome to JFrog swampUP 2018! :)" );
   } );
   server.begin();
   _lastOTACheck = millis();
